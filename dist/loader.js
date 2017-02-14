e55d6d2ebbcf1337c583539b0f812e324f2540b4
Pavel Vojacek <pavel.vojacek@fragaria.cz>
2017-02-14 11:10:01 +0100

    Strip src/ from filename.

--- dist/loader.js
+++ dist/loader.js
@@ -47,7 +47,7 @@ function extractTranslations(loader, source, sourceMaps) {
     var options = loaderUtils.parseQuery(this.query);
     var extractor = new angular_gettext_tools_1.Extractor(options);
     var root = findRoot(this.options.context, this.options.entry.main);
-    var filename = path.relative(root, this.resourcePath).replace(/^(\.\.\/)*(node_modules\/)?/, '');
+    var filename = path.relative(root, this.resourcePath).replace(/^(\.\.\/)*(node_modules\/)?(src\/)?/, '');
     extractor.parse(filename, source);
     loader.pruneGettextStrings(filename);
     loader.addGettextStrings(_.clone(extractor.strings));
